927ae44501ab2ce96e6885a90421a5626c9f5317
desman012 <desmandwisaputra@gmail.com>
2024-09-17 14:40:06 +0700

    Update file tugas pert3

--- src/konversi/KonversiSuhu.java
+++ src/konversi/KonversiSuhu.java
@@ -1,11 +1,11 @@
 package konversi;
 public class KonversiSuhu {
-    public void celciusToFahrenheit(int a){
-        int hasil = a*9/5+32;
-        System.out.println("Hasil konversi nilai "+a+" Celcius adalah : "+hasil+" Fahrenheit");        
+    public void celciusToFahrenheit(float celcius){
+        float hasil = celcius*9/5+32;
+        System.out.println("Hasil konversi nilai "+celcius+" Celcius adalah : "+hasil+" Fahrenheit");        
     }
-    public void celciusToReamur(int a){
-        int hasil = a*4/5;
-        System.out.println("Hasil konversi nilai "+a+" Celcius adalah : "+hasil+" Reamur");
+    public void celciusToReamur(float celcius){
+        float hasil = celcius*4/5;
+        System.out.println("Hasil konversi nilai "+celcius+" Celcius adalah : "+hasil+" Reamur");
     }
 }
--- src/konversi/KonversiSuhu2.java
+++ src/konversi/KonversiSuhu2.java
@@ -1,8 +1,8 @@
 package konversi;
 public class KonversiSuhu2 extends KonversiSuhu{
-    public void fahrenheitToReamur(int a){
-        int hasil = (a-32)*4/9;
-        System.out.println("Hasil konversi nilai "+a+" Fahrenheit adalah : "+hasil+" Reamur");
+    public void fahrenheitToReamur(float fahrenheit){
+        float hasil = (fahrenheit-32)*4/9;
+        System.out.println("Hasil konversi nilai "+fahrenheit+" Fahrenheit adalah : "+hasil+" Reamur");
 
     }
 }
